f539940c241058802dc15ad7ed9d0c46e9c30a35
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2025-04-17 10:27:46 -0700

    etc

--- src/Minipat/Octune/Ast.hs
+++ src/Minipat/Octune/Ast.hs
@@ -1,57 +1,61 @@
 module Minipat.Octune.Ast where
 
-import Bowtie (Fix)
+import Bowtie.Anno (Anno)
+import Bowtie.Memo (Memo)
 import Data.Sequence (Seq)
 import Data.Text (Text)
+import Looksee (Span)
+
+type Loc = Span Int
 
 data Waveform
   = Square
   | Sawtooth
   | Triangle
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 data Fun
-  = -- Sequence of samples
-    FunSeq
-  | -- Layering samples
+  = -- | Sequence of samples
+    FunSequ
+  | -- | Layering samples
     FunMerge
-  | -- Repeating sequence of samples
-    FunRepeat !Int
-  | -- Set waveform to use
+  | -- | Repeating sequence of samples
+    FunRepeat !Rational
+  | -- | Set waveform to use
     FunWaveform !Waveform
-  | -- Set amplitude of generated samples
+  | -- | Set amplitude of generated samples
     FunVolume !Rational
-  | -- Take the subsection of a sequence of samples between
-    --   the ends of the given beats
+  | -- | Take the subsection of a sequence of samples between the ends of the given beats
     -- Note: the end of the 0th beat is the beginning of the 1st beat
     FunSlice !Beats !Beats
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 data QualName = QualName
   { qnModule :: !(Seq Text)
   , qnVar :: !Text
   }
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
-data AstF r
-  = AstFile !(Seq Text) !(Seq r)
-  | AstDecl !Text r
-  | AstSong !Rational r
-  | AstVar !QualName
-  | AstNote !Note
-  | AstApp !Fun !(Seq r)
-  deriving (Eq, Ord, Show)
+data FileF r = File !(Seq Text) !(Seq r)
+  deriving stock (Eq, Ord, Show)
 
-type Ast = Fix AstF
+type File = Anno Loc (FileF Decl)
 
-data CoreF r
-  = CoreSong !Rational r
-  | CoreVar !QualName
-  | CoreNote !Note
-  | CoreApp !Fun !(Seq r)
-  deriving (Eq, Ord, Show)
+data DeclF r
+  = DeclSong !Text !Rational r
+  | DeclPart !Text r
+  deriving stock (Eq, Ord, Show)
 
-type Core = Fix CoreF
+type Decl = Anno Loc (DeclF Exp)
+
+data ExpF r
+  = ExpVar !QualName
+  | ExpNote !Note
+  | ExpApp !Fun !(Seq r)
+  | ExpCheck !Beats r
+  deriving stock (Eq, Ord, Show)
+
+type Exp = Memo ExpF Loc
 
 data Letter
   = C
@@ -61,33 +65,32 @@ data Letter
   | G
   | A
   | B
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 data Accidental
   = Flat
   | Sharp
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 type Octave = Int
 
 data Percussion
   = Snare
   | Clap
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 data Sound
   = Pitch !Letter !(Maybe Accidental) !Octave
   | Drum !Percussion
   | Rest
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
--- TODO: more modifiers
 data NoteModifier
   = Detached
   | Staccato
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
 
 type Beats = Rational
 
 data Note = Note !(Seq NoteModifier) !Beats !Sound
-  deriving (Eq, Ord, Show)
+  deriving stock (Eq, Ord, Show)
--- src/Minipat/Octune/Parser.hs
+++ src/Minipat/Octune/Parser.hs
@@ -2,6 +2,16 @@
 
 module Minipat.Octune.Parser where
 
+import Bowtie.Anno (Anno (..))
+import Bowtie.Memo (Memo, pattern MemoP)
+import Control.Applicative ((<|>))
+import Control.Exception (throwIO)
+import Data.Char (isAlphaNum, isLower, isUpper)
+import Data.Maybe (fromMaybe)
+import Data.Sequence (Seq (..))
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
 import Data.Void (Void)
 import Looksee qualified as L
 import Looksee.Lexer qualified as LL
@@ -90,3 +100,191 @@ lexP =
 
 noteP :: P Note
 noteP = lexP (Note <$> L.repeatP noteModifierP <*> beatsP <*> soundP)
+
+satP :: String -> (Char -> Bool) -> P Char
+satP msg p = do
+  c <- L.headP
+  if p c
+    then pure c
+    else fail msg
+
+upperCharP :: P Char
+upperCharP = satP "not upper case" isUpper
+
+lowerCharP :: P Char
+lowerCharP = satP "not lower case" isLower
+
+lowerTextP :: P Text
+lowerTextP = L.takeWhileP isLower
+
+identTrailP :: P Text
+identTrailP = L.takeWhileP (\c -> isAlphaNum c || c == '#')
+
+moduleComponentP :: P Text
+moduleComponentP = T.cons <$> upperCharP <*> lowerTextP
+
+identP :: P Text
+identP = T.cons <$> lowerCharP <*> identTrailP
+
+moduleSymP :: P ()
+moduleSymP = lexP (L.textP_ "module")
+
+moduleNameP :: P (Seq Text)
+moduleNameP = L.sepByP (L.charP_ '.') moduleComponentP
+
+moduleDeclP :: P (Seq Text)
+moduleDeclP = lexP (moduleSymP *> moduleNameP)
+
+qualNameP :: P QualName
+qualNameP = lexP $ do
+  m <- fmap (fromMaybe Empty) $ L.optP $ do
+    m <- moduleNameP
+    L.charP_ '.'
+    pure m
+  fmap (QualName m) identP
+
+annoP :: P a -> P (Anno Loc a)
+annoP = L.spanAroundP Anno
+
+memoP :: P (f (Memo f Loc)) -> P (Memo f Loc)
+memoP = L.spanAroundP MemoP
+
+openSongP
+  , closeSongP
+  , openSequP
+  , closeSequP
+  , openRepeatP
+  , closeRepeatP
+  , openMergeP
+  , closeMergeP
+  , openUsingWfP
+  , closeUsingWfP
+  , openVolumeP
+  , closeVolumeP
+  , openSliceP
+  , closeSliceP
+  , openCheckP
+  , closeCheckP
+  , colonP
+  , equalsP
+  , tildeP
+    :: P ()
+openSongP = lexP (L.charP_ '{')
+closeSongP = lexP (L.charP_ '}')
+openSequP = lexP (L.charP_ '[')
+closeSequP = lexP (L.charP_ ']')
+openRepeatP = lexP (L.textP_ "[*")
+closeRepeatP = lexP (L.textP_ "*]")
+openMergeP = lexP (L.textP_ "[+")
+closeMergeP = lexP (L.textP_ "+]")
+openUsingWfP = lexP (L.textP_ "[^")
+closeUsingWfP = lexP (L.textP_ "^]")
+openVolumeP = lexP (L.textP_ "[!")
+closeVolumeP = lexP (L.textP_ "!]")
+openSliceP = lexP (L.textP_ "[-")
+closeSliceP = lexP (L.textP_ "-]")
+openCheckP = lexP (L.textP_ "[#")
+closeCheckP = lexP (L.textP_ "#]")
+colonP = lexP (L.charP_ ':')
+equalsP = lexP (L.charP_ '=')
+tildeP = lexP (L.charP_ '=')
+
+fileP :: P File
+fileP = lexP (annoP (File <$> moduleDeclP <*> L.repeatP declP))
+
+declP :: P Decl
+declP = songP <|> partP
+
+songP :: P Decl
+songP = lexP $ annoP $ do
+  ident <- lexP identP
+  equalsP
+  L.betweenP openSongP closeSongP $ do
+    bpm <- fmap fromInteger (lexP L.uintP)
+    colonP
+    fmap (DeclSong ident bpm) expP
+
+partP :: P Decl
+partP = lexP $ annoP $ do
+  ident <- lexP identP
+  equalsP
+  fmap (DeclPart ident) expP
+
+expP :: P Exp
+expP = L.altP [noteExpP, checkExpP, appExpP, varExpP]
+
+noteExpP :: P Exp
+noteExpP = lexP (memoP (ExpNote <$> noteP))
+
+checkExpP :: P Exp
+checkExpP = lexP $ memoP $ L.betweenP openCheckP closeCheckP $ do
+  beats <- lexP L.udecP
+  colonP
+  fmap (ExpCheck beats) expP
+
+varExpP :: P Exp
+varExpP = lexP (memoP (ExpVar <$> qualNameP))
+
+appExpP :: P Exp
+appExpP =
+  L.altP
+    [ appRepeatP
+    , appMergeP
+    , appChordP
+    , appUsingWfP
+    , appVolumeP
+    , appSliceP
+    , appSequP
+    ]
+
+appRepeatP :: P Exp
+appRepeatP = lexP $ memoP $ L.betweenP openRepeatP closeRepeatP $ do
+  times <- lexP L.udecP
+  colonP
+  fmap (ExpApp (FunRepeat times)) (L.repeatP expP)
+
+appMergeP :: P Exp
+appMergeP = lexP $ memoP $ L.betweenP openMergeP closeMergeP $ do
+  fmap (ExpApp FunMerge) (L.repeatP expP)
+
+appChordP :: P Exp
+appChordP = lexP $ memoP $ L.betweenP openMergeP closeMergeP $ do
+  nodeMods <- L.repeatP noteModifierP
+  beats <- lexP L.udecP
+  colonP
+  pitches <- L.repeatP (lexP (annoP soundP))
+  let notes = flip fmap pitches $ \(Anno loc n) ->
+        MemoP loc (ExpNote (Note nodeMods beats n))
+  pure (ExpApp FunMerge notes)
+
+appUsingWfP :: P Exp
+appUsingWfP = lexP $ memoP $ L.betweenP openUsingWfP closeUsingWfP $ do
+  wf <- lexP waveformP
+  colonP
+  fmap (ExpApp (FunWaveform wf)) (L.repeatP expP)
+
+appVolumeP :: P Exp
+appVolumeP = lexP $ memoP $ L.betweenP openVolumeP closeVolumeP $ do
+  vol <- lexP L.udecP
+  colonP
+  fmap (ExpApp (FunVolume vol)) (L.repeatP expP)
+
+appSliceP :: P Exp
+appSliceP = lexP $ memoP $ L.betweenP openSliceP closeSliceP $ do
+  start <- lexP L.udecP
+  tildeP
+  end <- lexP L.udecP
+  colonP
+  fmap (ExpApp (FunSlice start end)) (L.repeatP expP)
+
+appSequP :: P Exp
+appSequP = lexP $ memoP $ L.betweenP openSequP closeSequP $ do
+  fmap (ExpApp FunSequ) (L.repeatP expP)
+
+parseI :: P a -> String -> IO a
+parseI p s = L.parseI p (T.pack s) >>= either throwIO pure
+
+parseF :: FilePath -> IO File
+parseF fp = do
+  contents <- TIO.readFile fp
+  either throwIO pure (L.parse fileP contents)
